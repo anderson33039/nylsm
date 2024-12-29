ce606a535f4f644c77ee3c0eac2f1c400273408f
anderson33039 <anderson.j4907@gmail.com>
2024-12-29 22:45:26 +0800

    Add files via upload

---
+++ assets/compound.js
@@ -0,0 +1,53 @@
+const list = {
+    'codes' : [
+        {
+            "name" : "Shane",
+            "code" : 1386495762
+        },
+        {
+            "name" : "Jobelle",
+            "code" : 7346895683
+        },
+        {
+            "name" : "Catherine",
+            "code" : 1165253846
+        },
+        {
+            "name" : "Sophia",
+            "code" : 5267385946
+        },
+        {
+            "name" : "Trio",
+            "code" : 4658352659
+        },
+        
+    ]
+};
+function codename(int) {
+    const result = list.codes.find(era => era.code === parseInt(int));
+    return result ? result.name : "Invitation code not found";
+} 
+document.getElementById("submit").addEventListener('click', (e) => {
+    e.preventDefault();
+    const given = document.getElementById('given').value;
+    const reso = codename(given);
+    
+    if(reso == 'Catherine'){
+        window.location.href = 'messages/catherine.html';
+    }
+    else if(reso == 'Jobelle'){
+        window.location.href = 'messages/jobelle.html';
+    }
+    else if(reso == 'Shane'){
+        window.location.href = 'messages/shane.html';
+    }
+    else if(reso == 'Sophia'){
+        window.location.href = 'messages/sophia.html';
+    }
+    else if(reso == 'Trio'){
+        window.location.href = 'messages/trio.html';
+    }
+    else {
+        alert(reso);
+    }
+});
